35d86f13bf83caf60ebe9b524daa9c592c838ccb
Sébastien <niou128@gmail.com>
2018-09-09 16:39:18 +0200

    Get sleep time

--- ConvertAutoHotkeyToMacroRazer/Helpers/ParseHelper.cs
+++ ConvertAutoHotkeyToMacroRazer/Helpers/ParseHelper.cs
@@ -24,5 +24,100 @@ namespace ConvertAutoHotkeyToMacroRazer.Helpers
 
             return listElt;
         }
+
+        public static InsertType GetType(string entry)
+        {
+            if (entry.StartsWith("SendInput", true, null))
+            {
+                return InsertType.SendInput;
+            }
+            if (entry.StartsWith("Sleep", true, null))
+            {
+                return InsertType.Sleep;
+            }
+
+            return InsertType.Undefined;
+        }
+
+        public static string GetKeyPressed(string entry)
+        {
+            string result = String.Empty;
+            string key = String.Empty;
+            string pattern = @"{Numpad([\d]+)[\w ]*}";
+            Match match = Regex.Match(entry, pattern, RegexOptions.IgnoreCase);
+            if (match.Success)
+            {
+                key = match.Groups[1].Value;
+            }
+            switch (key)
+            {
+                case "1":
+                    result = EnumKey.Key1.GetDescription();
+                    break;
+                case "2":
+                    result = EnumKey.Key2.GetDescription();
+                    break;
+                case "3":
+                    result = EnumKey.Key3.GetDescription();
+                    break;
+                case "4":
+                    result = EnumKey.Key4.GetDescription();
+                    break;
+                case "5":
+                    result = EnumKey.Key5.GetDescription();
+                    break;
+                case "6":
+                    result = EnumKey.Key6.GetDescription();
+                    break;
+                case "7":
+                    result = EnumKey.Key7.GetDescription();
+                    break;
+                case "8":
+                    result = EnumKey.Key8.GetDescription();
+                    break;
+                case "9":
+                    result = EnumKey.Key9.GetDescription();
+                    break;
+                case "0":
+                    result = EnumKey.Key0.GetDescription();
+                    break;
+            }
+            return result;
+        }
+
+        public static int GetState(string entry)
+        {
+            int state = 2;
+            string pattern = @"[\w\d{]+[ ]([\w]+)}";
+            Match match = Regex.Match(entry, pattern);
+
+            if (match.Success)
+            {
+                string result = match.Groups[1].Value;
+                if (result.ToLower() == "Down".ToLower())
+                {
+                    state = 0;
+                }
+                if (result.ToLower() == "Up".ToLower())
+                {
+                    state = 1;
+                }
+
+            }
+            return state;
+        }
+
+        public static int GetTimeSleep(string entry)
+        {
+            int delay = 0;
+            string pattern = @"[, ]([\d]+)";
+            Match match = Regex.Match(entry, pattern);
+            if (match.Success)
+            {
+               Int32.TryParse(match.Groups[1].Value, out delay);
+            }
+
+            return delay;
+        }
     }
 }
--- XUnitTestProject1/ParseHelperTest.cs
+++ XUnitTestProject1/ParseHelperTest.cs
@@ -1,3 +1,4 @@
+using ConvertAutoHotkeyToMacroRazer;
 using ConvertAutoHotkeyToMacroRazer.Helpers;
 using System;
 using Xunit;
@@ -55,6 +56,84 @@ SendInput {Numpad3 down}";
             var result = ParseHelper.ParseElt(entry);
 
             Assert.Empty(result);
+        }
+
+        [Fact]
+        public void If_SendIput_return_type_Sendinput()
+        {
+            string entry = "SendInput {Numpad2 down}";
+
+            var result = ParseHelper.GetType(entry);
+
+            Assert.Equal(InsertType.SendInput, result);
+        }
+
+        [Fact]
+        public void If_sleep_return_type_Sleep()
+        {
+            string entry = "Sleep, 75";
+
+            var result = ParseHelper.GetType(entry);
+
+            Assert.Equal(InsertType.Sleep, result);
+        }
+
+        [Fact]
+        public void If_Key_press_is_1_return_07001E()
+        {
+            string entry = "SendInput {Numpad1 down}";
+
+            var result = ParseHelper.GetKeyPressed(entry);
+
+            Assert.Equal("07001E", result);
+        }
+
+        [Theory]
+        [InlineData("SendInput {Numpad1 down}", "07001E")]
+        [InlineData("SendInput {Numpad2 down}", "07001F")]
+        [InlineData("SendInput {Numpad3 down}", "070020")]
+        [InlineData("SendInput {Numpad4 down}", "070021")]
+        [InlineData("SendInput {Numpad5 down}", "070022")]
+        [InlineData("SendInput {Numpad6 down}", "070023")]
+        [InlineData("SendInput {Numpad7 down}", "070024")]
+        [InlineData("SendInput {Numpad8 down}", "070025")]
+        [InlineData("SendInput {Numpad9 down}", "070026")]
+        [InlineData("SendInput {Numpad0 down}", "070027")]
+        public void Return_Correct_Razer_Key_Code(string input, string expected)
+        {
+            var result = ParseHelper.GetKeyPressed(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void If_key_down_return_state_0()
+        {
+            string entry = "SendInput {Numpad1 down}";
+
+            var result = ParseHelper.GetState(entry);
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void If_Key_up_return_state_1()
+        {
+            string entry = "SendInput {Numpad1 Up}";
+
+            var result = ParseHelper.GetState(entry);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void If_Sleep_300_return_300()
+        {
+            string entry = "Sleep, 300";
+
+            var result = ParseHelper.GetTimeSleep(entry);
+
+            Assert.Equal(300, result);
         }
     }
 }
