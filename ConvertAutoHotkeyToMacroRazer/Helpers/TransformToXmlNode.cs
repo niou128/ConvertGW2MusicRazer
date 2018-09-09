299c920d10f95a35c4b60714e777ae08cddd76a6
Sébastien <niou128@gmail.com>
2018-09-09 19:40:44 +0200

    Create node delay and node keyboard

--- ConvertAutoHotkeyToMacroRazer/Helpers/TransformToXmlNode.cs
+++ ConvertAutoHotkeyToMacroRazer/Helpers/TransformToXmlNode.cs
@@ -2,11 +2,36 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Xml;
 
 namespace ConvertAutoHotkeyToMacroRazer.Helpers
 {
     public static class TransformToXmlNode
     {
-        
+        public static XmlDocument CreateNodeDelay(string entry)
+        {
+            string NodeDelay = String.Empty;
+            int delay = ParseHelper.GetTimeSleep(entry);
+
+            NodeDelay = $@"<Event id=""DELAY"">{Environment.NewLine}<Delay>{delay}</Delay>{Environment.NewLine}</Event>";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(NodeDelay);
+
+            return doc;
+        }
+
+        public static XmlDocument CreateNodeKeyboard(string entry)
+        {
+            string NodeKeyboard = String.Empty;
+
+            var key = ParseHelper.GetKeyPressed(entry);
+            var state = ParseHelper.GetState(entry);
+            NodeKeyboard = $@"<Event id =""KEYBOARD""><HID>{key}</HID><State>{state}</State></Event>";
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(NodeKeyboard);
+
+            return doc;
+        }
     }
 }
--- XUnitTestProject1/TransformToXmlNodeTest.cs
+++ XUnitTestProject1/TransformToXmlNodeTest.cs
@@ -4,11 +4,51 @@ using System.Text;
 using Xunit;
 using ConvertAutoHotkeyToMacroRazer.Helpers;
 using ConvertAutoHotkeyToMacroRazer;
+using System.Xml;
 
 namespace XUnitTestProject1
 {
     public class TransformToXmlNodeTest
     {
-        
+        [Fact]
+        public void If_Event_Is_Sleep_Return_Node_delay()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml($@"<Event id=""DELAY""><Delay>300</Delay></Event>");
+
+            var expected = doc;
+
+            string entry = @"Sleep, 300";
+
+            var result = TransformToXmlNode.CreateNodeDelay(entry);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void If_Event__is_input_key_1_down_return_Node_Keyboard_state_0()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml($@"<Event id =""KEYBOARD""><HID>07001E</HID><State>0</State></Event>");
+
+            string entry = "SendInput {Numpad1 down}";
+
+            var result = TransformToXmlNode.CreateNodeKeyboard(entry);
+
+            Assert.Equal(doc.OuterXml, result.OuterXml);
+        }
+
+        [Fact]
+        public void If_Event__is_input_key_1_up_return_Node_Keyboard_state_1()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml($@"<Event id =""KEYBOARD""><HID>07001E</HID><State>1</State></Event>");
+
+            string entry = "SendInput {Numpad1 up}";
+
+            var result = TransformToXmlNode.CreateNodeKeyboard(entry);
+
+            Assert.Equal(doc.OuterXml, result.OuterXml);
+        }
     }
 }
